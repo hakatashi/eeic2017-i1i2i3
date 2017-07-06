898c98ccbedf8e4c8da407591f6ce9e5b7c798e0
Koki Takahashi <hakatasiloving@gmail.com>
2017-07-06 15:02:47 +0900

    Implement icmp send procedure

--- 08/icmp_send.c
+++ 08/icmp_send.c
@@ -14,18 +14,36 @@
 #include <netinet/ip_icmp.h>
 #include <netinet/tcp.h>
 
-#define PACKETSIZE 256
+#define PACKETSIZE (1024 * 20)
 
 struct packet {
 	struct icmphdr hdr;
-	char msg[PACKETSIZE - sizeof(struct icmphdr)];
+	char msg[PACKETSIZE];
 };
 
 struct receive_packet {
 	struct ip ip_header;
 	struct icmphdr hdr;
-	char msg[PACKETSIZE - sizeof(struct icmphdr)];
+	char msg[PACKETSIZE];
 };
+
+/* fd から 必ず n バイト読み, bufへ書く.
+	n バイト未満でEOFに達したら, 残りは0で埋める.
+	fd から読み出されたバイト数を返す */
+ssize_t read_n(int fd, ssize_t n, void * buf) {
+	ssize_t re = 0;
+	while (re < n) {
+		ssize_t r = read(fd, buf + re, n - re);
+		if (r == -1) {
+			perror("read");
+			exit(1);
+		}
+		if (r == 0) break;
+		re += r;
+	}
+	memset(buf + re, 0, n - re);
+	return re;
+}
 
 unsigned short checksum(void *b, int len) {
 	unsigned short *buf = b;
@@ -42,16 +60,8 @@ unsigned short checksum(void *b, int len) {
 	return result;
 }
 
-int main(int argc, char const *argv[]) {
-	int ret, i;
-
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <host>\n", argv[0]);
-		exit(1);
-	}
-
-	const char *host = argv[1];
-
+void ping(const char *host, const char *buf, int size) {
+	int ret;
 	struct sockaddr_in address;
 	address.sin_family = AF_INET;
 	address.sin_port = 0;
@@ -85,12 +95,9 @@ int main(int argc, char const *argv[]) {
 	packet_id.hdr.type = ICMP_ECHO;
 	const int pid = getpid();
 	packet_id.hdr.un.echo.id = pid;
-	printf("pid: %d\n", pid);
 
 	memset(packet_id.msg, 0, sizeof(packet_id.msg));
-	strcpy(packet_id.msg, "hello google");
-	printf("***sent message!***\n");
-	printf("%s\n", packet_id.msg);
+	memcpy(packet_id.msg, buf, size);
 
 	packet_id.hdr.un.echo.sequence = 0;
 	packet_id.hdr.checksum = checksum(&packet_id, sizeof(packet_id));
@@ -99,15 +106,29 @@ int main(int argc, char const *argv[]) {
 		perror("sendto");
 		return;
 	}
+}
 
-	sleep(1);
+int main(int argc, char const *argv[]) {
+	int ret, i;
 
-	struct sockaddr_in receive_address;
-	struct receive_packet receive_packet_id;
-	int receive_address_len = sizeof(receive_address);
-	if (recvfrom(socket_id, &receive_packet_id, sizeof(receive_packet_id), 0, (struct sockaddr*)&receive_address, &receive_address_len) > 0) {
-		printf("***got message!***\n");
-		printf("%s\n", receive_packet_id.msg);
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <host>\n", argv[0]);
+		exit(1);
+	}
+
+	const char *host = argv[1];
+
+	char buf[PACKETSIZE];
+	while (1) {
+		ret = read_n(0, PACKETSIZE, buf);
+		printf("read: %d bytes\n", ret);
+
+		if (ret == 0) {
+			break;
+		}
+
+		ping(host, buf, PACKETSIZE);
+		usleep(200 * 1000);
 	}
 
 	return 0;
